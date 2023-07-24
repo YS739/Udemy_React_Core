d15f864d986c085bd0f9e8ac758d9ea9dd027e5f
YS739 <suk921@gmail.com>
2023-07-24 17:03:45 +0900

    add:Link로 페이지 이동

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,9 +1,15 @@
 import classes from "./Modal.module.css";
+import { useNavigate } from "react-router-dom";
 
-function Modal({ children, onClose }) {
+function Modal({ children }) {
+  const navigate = useNavigate();
+
+  function closeHandler() {
+    navigate(".."); // 한 단계 위로 -> 여기서는 '/'로 가게 됨
+  }
   return (
     <>
-      <div className={classes.backdrop} onClick={onClose} />
+      <div className={classes.backdrop} onClick={closeHandler} />
       <dialog open className={classes.modal}>
         {children}
       </dialog>
